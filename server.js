d272610a405b28dec80d5a0daf1b4e232c08f77f
이가은 <gaeun.lee@mirero.co.kr>
2023-11-28 13:27:03 +0900

    도커 파일과 컴포즈 성공

--- server.js
+++ server.js
@@ -1,29 +1,29 @@
-const express = require("express");
-const path = require("path");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+// const express = require("express");
+// const path = require("path");
+// const cors = require("cors");
+// const bodyParser = require("body-parser");
+// const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const app = express();
+// const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: true, credentials: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(cors({ origin: true, credentials: true }));
 
-app.use(
-  ["/api"],
-  createProxyMiddleware({
-    target: "http://localhost:5123/",
-    changeOrigin: true,
-  })
-);
+// app.use(
+//   ["/api"],
+//   createProxyMiddleware({
+//     target: "http://localhost:5123/",
+//     changeOrigin: true,
+//   })
+// );
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "/build")));
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname, "/build")));
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/build/index.html"));
-});
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "/build/index.html"));
+// });
 
-app.listen(8080, function () {
-  console.log("listening on 8080");
-});
+// app.listen(8080, function () {
+//   console.log("listening on 8080");
+// });
